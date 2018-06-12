84b20728fa7a738febf2debf2caab922ea2788d5
Fatemeh Rezapoor <rezapoor.fa@gmail.com>
2018-06-11 20:59:29 -0600

    Write the code in one line. Added another method.

--- PythonExercise/Ex5.py
+++ PythonExercise/Ex5.py
@@ -3,8 +3,21 @@
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 c = []
+d=[]
+e=[]
 for i in range(0, len(a)):
     if a[i] in b:
         c.append(a[i])
 
 print(list(set(c)))  # set is sth which can not have any duplicates
+
+# Another method to write this code
+for x in a:
+    if x in b:
+        d.append(x)
+
+print(list(set(d)))
+
+# How to write this code in 1 line
+new=set([x for x in a if x in b])
+print(new)
